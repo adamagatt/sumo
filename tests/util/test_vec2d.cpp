aa9e26e0b2ef89acc49b9e66e1146833f65271cf
adam.gatt <adam.gatt@penguin.lxd>
2020-10-17 13:58:29 +1030

    Add parametrised testing

--- tests/util/test_vec2d.cpp
+++ tests/util/test_vec2d.cpp
@@ -1,12 +1,21 @@
-#include "../../src/util/vec2d.h"
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
-TEST (Vector, Add) { 
-    Vec2d vec1{1, 2};
-    Vec2d vec2{3, 4};
-    Vec2d result{4, 6};
-    ASSERT_EQ(vec1+vec2, result);
+#include "../../src/util/vec2d.h"
+
+class VectorTests :public ::testing::TestWithParam<std::tuple<Vec2d, Vec2d, Vec2d>> { };
+
+TEST_P(VectorTests, Add) {
+    ASSERT_EQ(std::get<0>(GetParam()) + std::get<1>(GetParam()), std::get<2>(GetParam()));
 }
+
+INSTANTIATE_TEST_CASE_P(
+  Vector,
+  VectorTests,
+  ::testing::Values(
+    std::make_tuple(Vec2d(1, 2), Vec2d(3, 4), Vec2d(4, 6)),
+    std::make_tuple(Vec2d(5, 5), Vec2d(-5, -5), Vec2d(0, 0))
+  )
+);
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
